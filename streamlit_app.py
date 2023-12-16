4698eb94cc41f33aa7193fe1914092884ae2a882
skamp931 <43207451+skamp931@users.noreply.github.com>
2023-12-16 23:53:21 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,24 +2,8 @@ import streamlit as st
 import time
 
 
-st.markdown(
-    """
-    <style>
-    backgroundColor = "#F000F0"
-
-    .reportview-container {
-        background: url("https://jp.freepik.com/free-photo/image-of-linear-slingshots-on-a-green-background_91607832.htm");
-    }
-   </style>
-    """,
-    unsafe_allow_html=True
-)
-
-st.markdown(
-    """
-    <link rel="stylesheet" type="text/css" href="https://www.example.com/style.css">
-    """,
-    unsafe_allow_html=True
-)
-
 st.write("テストデータ記載します")
+st.header('Configuration： Theme')
+st.checkbox(label='Check')
+st.slider(label='Slider')
+st.selectbox(label='Choose Your Favourite', options=['Snowpeak', 'Rakuten', 'Toyota Motors'])
